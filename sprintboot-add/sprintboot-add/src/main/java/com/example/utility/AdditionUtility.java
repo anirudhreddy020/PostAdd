977d4b6a97252aa1813105acd4fe87a88016532f
anirudhreddy020 <92798953+anirudhreddy020@users.noreply.github.com>
2023-06-25 12:55:15 -0500

    Update AdditionUtility.java
    Added a private constructor to hide the implicit public one in additionutility

--- sprintboot-add/sprintboot-add/src/main/java/com/example/utility/AdditionUtility.java
+++ sprintboot-add/sprintboot-add/src/main/java/com/example/utility/AdditionUtility.java
@@ -1,6 +1,10 @@
 package com.example.utility;
 
 public class AdditionUtility {
+
+    private AdditionUtility(){
+        
+    }
     public static int add(int a, int b) {
         return a + b;
     }
